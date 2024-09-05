ce665782716b08c6474efb085551d64efff43207
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-09-05 09:41:00 +0600

    update

---
+++ src/Store/Auth_Slice/index.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
+};
+
+const authslice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {},
+  },
+});
+export const { setUser } = authslice.actions;
+export default authslice.reducer;
